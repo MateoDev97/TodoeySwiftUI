fd3155a5acdeb7945835347931eaebe7a767bf38
Mateo Ortiz <mateo972000@gmail.com>
2023-02-26 17:58:04 -0500

    Initial Commit

---
+++ Todoey/TodoeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoeyApp.swift
+//  Todoey
+//
+//  Created by Mateo Ortiz on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
